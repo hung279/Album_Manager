8617a642f07fc1ad5f95784df75bf27a2de5d729
hung279 <hungnher279@gmail.com>
2023-11-06 22:58:02 +0700

    :wrench: Config basic database

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5439,
+  username: 'postgres',
+  password: 'hungyb123',
+  database: 'album_manager',
+  entities: [__dirname + '/../**/*.entity.ts'],
+  autoLoadEntities: true,
+  synchronize: true,
+};
