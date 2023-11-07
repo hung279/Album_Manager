658b1b7e34140ef9d11e2a8d76f324dc33176ef6
hung279 <hungnher279@gmail.com>
2023-11-07 10:01:04 +0700

    :memo: Update API docs Sign up and confirm email

--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,20 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class SignUpDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   readonly name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   readonly username: string;
 
+  @ApiProperty()
   @IsString()
   @MaxLength(30)
   @MinLength(8)
   @IsNotEmpty()
   readonly password: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   readonly email: string;
