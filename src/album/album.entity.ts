0033e95be22be8677e1885856ef68cbc733410f9
hung279 <hungnher279@gmail.com>
2023-11-08 00:13:46 +0700

    🔨 Create Album entity

---
+++ src/album/album.entity.ts
@@ -0,0 +1,23 @@
+import { BaseEntity } from 'src/common/entities/base.entity';
+import { Photo } from 'src/photo/photo.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Column, Entity, JoinTable, ManyToMany, OneToMany } from 'typeorm';
+
+@Entity()
+export class Album extends BaseEntity {
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: string;
+
+  @OneToMany(() => Photo, (photo) => photo.album)
+  photos: Photo[];
+
+  @ManyToMany(() => User)
+  @JoinTable()
+  users: User[];
+}
--- src/photo/photo.entity.ts
+++ src/photo/photo.entity.ts
@@ -1,3 +1,4 @@
+import { Album } from 'src/album/album.entity';
 import { BaseEntity } from 'src/common/entities/base.entity';
 import { User } from 'src/user/entities/user.entity';
 import { Column, Entity, ManyToOne } from 'typeorm';
@@ -15,6 +16,9 @@ export class Photo extends BaseEntity {
 
   @ManyToOne(() => User, (user) => user.photos)
   user: User;
+
+  @ManyToOne(() => Album, (album) => album.photos)
+  album: Album;
 
   @Column()
   status: string;
