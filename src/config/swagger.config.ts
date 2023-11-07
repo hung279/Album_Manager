6e36deb2709ad0b2316c9f0c3ed9715826b798ec
hung279 <hungnher279@gmail.com>
2023-11-07 09:50:05 +0700

    :memo: Add docs API swagger configuration

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,6 @@
+export const swaggerConfig = {
+  title: 'Album Manager',
+  description: 'Project Album Manager use NestJS',
+  version: '1.0',
+  path: 'docs',
+};
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { swaggerConfig } from './config/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port') || 8000;
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle(swaggerConfig.title)
+    .setDescription(swaggerConfig.description)
+    .setVersion(swaggerConfig.version)
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+
+  SwaggerModule.setup(swaggerConfig.path, app, document);
+
   await app.listen(port);
 }
 bootstrap();
